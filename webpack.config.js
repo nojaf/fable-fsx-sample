93d414f768f8a862d3398f60a3fc582b3b5c0fb4
nojaf <florian.verdonck@outlook.com>
2019-08-23 09:56:44 +0200

    Is working folder correctly?

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: './src/ReactTodoMVC.fsx',
+    entry:  path.join(__dirname, 'src', 'ReactTodoMVC.fsx'),
     output: {
         path: path.join(__dirname, "./public"),
         filename: "bundle.js",
